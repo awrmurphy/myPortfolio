f52c51e6340ca4684fbcc2728b5c3b28e5205b6d
awrmurphy <awrmurphy@gmail.com>
2025-07-14 18:09:19 -0400

    Added contact form with js validation and some css styling.

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function reveal(){
     x.style.display = 'inline';
   }
 }
+function validateForm(){
+    let x =document.forms["contactForm"]["name"].value;
+    if (x==""){
+        alert("Please enter something for me to refer to you by.");
+        return false;
+    }
+    let y =document.forms["contactForm"]["email"].value;
+    if (y==""){
+        alert("Please enter an email for me to reach out to you.");
+        return false;
+    }
+    if (!y.toLowerCase().match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+        alert("Please enter a VALID email for me to reach out to you.");
+        return false;
+    }
+    let z =document.forms["contactForm"]["message"].value;
+    if (z==null){
+        alert("Please enter a message to send to me.");
+        return false;
+    }
+    if (z.length<10){
+        alert("Please enter a message of at least 10 characters to a maximum of 200 characters.");
+        return false;
+    }
+    else{
+        return true
+    }
+}
